ce23141d476960bb1d516aac2545692889f7378d
Joan <jdamiaros@gmail.com>
2022-10-19 19:18:16 +0200

    .

--- Output/Assets/Maps/MapaBase.tsx
+++ Output/Assets/Maps/MapaBase.tsx
@@ -1,4 +1,4 @@
 <?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.9" tiledversion="1.9.2" name="MapaBase" tilewidth="16" tileheight="16" tilecount="330" columns="22">
- <image source="Tiles/terrain.png" width="352" height="240"/>
+<tileset version="1.9" tiledversion="1.9.2" name="MapaBase" tilewidth="32" tileheight="32" tilecount="132" columns="11">
+ <image source="Tiles/Tiles.png" width="352" height="384"/>
 </tileset>
