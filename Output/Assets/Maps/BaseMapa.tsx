8a5774f6b8b461db26c9b556d7ba14f67715a07a
Joan <jdamiaros@gmail.com>
2022-10-19 19:05:38 +0200

    .

--- Output/Assets/Maps/BaseMapa.tsx
+++
@@ -1,4 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.9" tiledversion="1.9.2" name="BaseMapa" tilewidth="32" tileheight="32" tilecount="77" columns="11">
- <image source="Tiles/terrain.png" width="352" height="240"/>
-</tileset>
