b37ea73a77a721de8f38bd8f83c299f7f3d80452
Joan <jdamiaros@gmail.com>
2022-10-19 18:26:19 +0200

    .

---
+++ Output/Assets/Maps/Tiles/basicTiles.tsx
@@ -0,0 +1,9 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.9" tiledversion="1.9.2" name="terrain" tilewidth="16" tileheight="16" tilecount="330" columns="22">
+ <image source="../../../../PlatformerGame/Output/Assets/Textures/Maps/terrain.png" width="352" height="240"/>
+ <wangsets>
+  <wangset name="Terrenos" type="corner" tile="-1">
+   <wangcolor name="Grass" color="#ff0000" tile="-1" probability="1"/>
+  </wangset>
+ </wangsets>
+</tileset>
