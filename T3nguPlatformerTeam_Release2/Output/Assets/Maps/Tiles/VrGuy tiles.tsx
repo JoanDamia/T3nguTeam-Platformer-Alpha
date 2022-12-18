c67cf494bb0488dc520f6dbeef97da9184606873
Joan <jdamiaros@gmail.com>
2022-12-18 17:17:31 +0100

    Carpeta Release2

---
+++ T3nguPlatformerTeam_Release2/Output/Assets/Maps/Tiles/VrGuy tiles.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.9" tiledversion="1.9.2" name="VrGuy tiles" tilewidth="32" tileheight="32" tilecount="77" columns="11">
+ <image source="../../Textures/Maps/terrain.png" width="352" height="240"/>
+</tileset>
